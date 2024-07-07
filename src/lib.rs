b6f44b1949f57d9d7cb1c74285d49007466192c1
Andrzej Hunt <andrzej@ahunt.org>
2024-07-07 13:00:48 +0200

    Remove useless generated boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,3 @@
 // TODO: temporarily make public until the real API has been determined.
 pub mod measurement;
 pub mod shdlc;
-
-// TODO: remove default boilerplate.
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
